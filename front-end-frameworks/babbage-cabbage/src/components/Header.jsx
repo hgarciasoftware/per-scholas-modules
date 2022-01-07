3557e04967b0f8147d244655866d627706139f44
Hector Garcia <hgarciasoftware@gmail.com>
2022-01-07 11:41:03 -0500

    babbage-cabbage: Improve print display

--- front-end-frameworks/babbage-cabbage/src/components/Header.jsx
+++ front-end-frameworks/babbage-cabbage/src/components/Header.jsx
@@ -1,10 +1,12 @@
 function Header(props) {
   return (
     <div className="header">
-      <h1>{props.companyName}</h1>
-      <img src={props.image} alt={props.eventName} />
-      <h2>{props.eventName}</h2>
-      <h3>{props.eventDate}</h3>
+      <div>
+        <h1>{props.companyName}</h1>
+        <img src={props.image} alt={props.eventName} />
+        <h2>{props.eventName}</h2>
+        <h3>{props.eventDate}</h3>
+      </div>
     </div>
   );
 }
