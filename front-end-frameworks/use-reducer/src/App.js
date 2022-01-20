6b5302c2da0f7f913e20f6c5c8064c05313c8cdb
Hector Garcia <hgarciasoftware@gmail.com>
2022-01-20 15:45:08 -0500

    use-reducer: copy in-class example

--- front-end-frameworks/use-reducer/src/App.js
+++ front-end-frameworks/use-reducer/src/App.js
@@ -1,9 +1,37 @@
+import { useReducer } from 'react';
 import './App.css';
 
+function reducer(state, action) {
+
+  switch (action.type) {
+  case 'add':
+    return {
+      list: [...state.list, action.payload]
+    };
+  case 'remove':
+    return {
+      list: state.list.filter(num => num !== action.payload)
+    };
+  default:
+    return state;
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, { list: [] });
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      My App
+      {state.list.map((num, i) => <p key={i} onClick={() => dispatch({ type: 'remove', payload: num })}>{num}</p>)}
+      <button onClick={() => dispatch({ type: 'add', payload: Math.random() })}>Add</button>
+    </div>
   );
 }
 
 export default App;
+
+
+  // 1. Call dispatch with an action
+  // 2. Action is sent to reducer function
+  // 3. reducer function returns new state
