f0f5e4ea54f5088ea461e8636be756972204b335
Hector Garcia <hgarciasoftware@gmail.com>
2022-01-20 11:20:54 -0500

    react-router: make use of `useNavigate`

--- front-end-frameworks/react-router/src/Contact.jsx
+++ front-end-frameworks/react-router/src/Contact.jsx
@@ -1,5 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+
 function Contact() {
-  return <h3>This is a Contact Us page</h3>;
+  let navigate = useNavigate();
+
+  return (
+    <div>
+      <h3>This is a Contact Us page</h3>
+      <button onClick={() =>{navigate('/home')}}>Go back to Home Page</button>
+    </div>
+  );
 }
 
 export default Contact;
